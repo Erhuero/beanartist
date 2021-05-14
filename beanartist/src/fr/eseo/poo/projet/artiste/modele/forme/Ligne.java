6336a609e01ad7c27b8d9f4358c0daf7d150eafd
Constantin <constantin.chtanko@etu.formation-aftec.com>
2021-05-14 16:10:42 +0200

    Add class Ligne + refactor

---
+++ beanartist/src/fr/eseo/poo/projet/artiste/modele/forme/Ligne.java
@@ -0,0 +1,18 @@
+package fr.eseo.poo.projet.artiste.modele.forme;
+
+public class Ligne extends Forme {
+	
+	public Ligne() {
+		
+	}
+	
+	public Ligne(double largeur, double hauteur) {
+		this.HAUTEUR_PAR_DEFAUT=largeur;
+		this.LARGEUR_PAR_DEFAUT=hauteur;
+		}
+	
+	
+}
+
+
+
