371b818c37a159ca5ef9d5fc05d427cd6de22089
Constantin <constantin.chtanko@etu.formation-aftec.com>
2021-06-22 17:49:35 +0200

    JFrame Pannel

--- beanartist/src/fr/eseo/poo/projet/artiste/vue/ihm/PanneauDessin.java
+++ beanartist/src/fr/eseo/poo/projet/artiste/vue/ihm/PanneauDessin.java
@@ -2,13 +2,16 @@ package fr.eseo.poo.projet.artiste.vue.ihm;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.SwingUtilities;
 import javax.swing.JPanel;
 
 public class PanneauDessin extends JPanel {
 	
-	public static final int LARGEUR_PAR_DEFAUT = 1450;
-	public static final int HAUTEUR_PAR_DEFAUT = 850;
-	public static final Color COULEUR_FOND_PAR_DEFAUT = Color.white;
+	public static final int LARGEUR_PAR_DEFAUT = 800;
+	public static final int HAUTEUR_PAR_DEFAUT = 480;
+	public static final Color COULEUR_FOND_PAR_DEFAUT = Color.WHITE;
+//	private final List<VueForme> vueFormes = new ArrayList<VueForme>();
 	public double largeur, hauteur;
 	
 	public PanneauDessin() {
@@ -16,11 +19,11 @@ public class PanneauDessin extends JPanel {
 	}
 	
 	public PanneauDessin(int largeur, int hauteur, Color color) {
-		
-		this.largeur = largeur;
-		this.hauteur = hauteur;
-		this.setBackground(color);
-		//this.COULEUR_FOND_PAR_DEFAUT = color;
+		super();
+		Dimension dimension = new Dimension(largeur, hauteur);
+		setPreferredSize(new Dimension(largeur, hauteur));
+		this.setBackground(COULEUR_FOND_PAR_DEFAUT);
+		setBackground(color);
 	}
 	
 	
--- beanartist/test/fr/eseo/poo/projet/artiste/vue/ihm/PanneauDessinTest.java
+++ beanartist/test/fr/eseo/poo/projet/artiste/vue/ihm/PanneauDessinTest.java
@@ -1,21 +1,41 @@
 package fr.eseo.poo.projet.artiste.vue.ihm;
+import java.awt.Color;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class PanneauDessinTest {
-
-	public static void main(String args[]) {
-		
 	
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new PanneauDessinTest();
+			}
+		});
+	}
+	
+	public PanneauDessinTest() {
+		this.testConstructeurParDefaut();
+		this.testConstructeur();
 	}
 	
 	private void testConstructeurParDefaut() {
-		
+		JFrame fenetre = new JFrame(new String("Etre un Artiste"));
+		fenetre.add(new PanneauDessin());
+		fenetre.pack();
+		fenetre.setVisible(true);
+		fenetre.setLocationRelativeTo(null);
 	}
 	
 	private void testConstructeur() {
-		
+		JFrame fenetre = new JFrame(new String("Blues du Businessman"));
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.add(new PanneauDessin(600,360, new Color(150,255,255)));
+		fenetre.pack();
+		fenetre.setVisible(true);
+		fenetre.setLocationRelativeTo(null);
 	}
+	
+	
 }
